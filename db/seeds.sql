2a6fcae59661e57fd5066050b0986a0cca5fa97c
mw18 <67554339+mw18@users.noreply.github.com>
2021-01-09 18:31:45 -0600

    add and write config and model filed

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Hamburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+
+
+
+/* OR 
+INSERT INTO colleges (name)
+VALUES ("Emerson"), ("Northeastern"), ("MIT");
+*/
